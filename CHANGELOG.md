2002ebaef4765ff7443c3d46189f4a1b1008f0c1
Tobias L. Maier <me@tobiasmaier.info>
2014-08-26 11:29:27 +0200

    Fix naming error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.0.2 (2014-08-26)
+
+* Fix naming error
+  `uninitialized constant Workflow::Sequel::Model`
+
 ## 0.0.1 (2014-08-26)
 
 * Initial release with support for [workflow 1.1.0](https://github.com/geekq/workflow/tree/v1.1.0)
--- lib/workflow/sequel.rb
+++ lib/workflow/sequel.rb
@@ -4,7 +4,7 @@ require 'workflow/adapter/sequel'
 module Workflow
   module Sequel
     def self.included(klass)
-      return unless Object.const_defined?('Sequel') && klass < Sequel::Model
+      return unless Object.const_defined?('Sequel') && klass < ::Sequel::Model
 
       klass.send :include, Adapter::Sequel
     end
